37a3faf7e956fa017fa5c4bb5ea8806e1e32b624
SIA-PC\huhongyan <huhongyan@sia.cn>
2022-01-17 10:48:55 +0800

    修改了一个bug，否则在source的最后一个文件名会多一个冒号

--- scripts/Bin2Bin_Score_Calculator/run_bin_sim.sh
+++ scripts/Bin2Bin_Score_Calculator/run_bin_sim.sh
@@ -10,7 +10,7 @@ Source_bins_folder=KnownLibs_FVs
 echo --- Calculating the similarity score between binaries listed in $(basename $Extracted_bins) and binariess in $(basename $Source_bins) ---
 for file1 in $(cat $Extracted_bins);
 do
-    for file2 in $(cat $Source_bins):
+    for file2 in $(cat $Source_bins);
     do
 	python3 scripts/Bin2Bin_Score_Calculator/binsimScore.py -f $Extracted_bins_folder/$file1 -f $Source_bins_folder/$file2
     done
